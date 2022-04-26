2a46356be4e7818eb8c7a706c7ff45fb240d1f97
Williams Douglas Jose dos Santos <wdjs@ug3c37.windows.cin.ufpe.br>
2022-04-26 14:23:01 -0300

    adicionando feature de scanning

--- RPNStack.java
+++ RPNStack.java
@@ -2,48 +2,63 @@ import java.util.Stack;
 import java.util.Scanner;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class RPNStack{
     //private static final Exception IOException = null;
 
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args) throws Exception {
         Stack<Integer> stack = new Stack<>();
+        Queue<String> queue = new LinkedList<>();
+
         String path = "caminho para o arquivo txt";
 
         String value = "";
         Scanner in = new Scanner(new FileReader(path)); //lendo o arquivo de texto
         System.out.println(value);
         int op1, op2;
-    
+        
         while(in.hasNext()){
            value = in.next();
            
+           if(value.matches("[+-]?\\d*(\\.\\d+)?")) {
+            stack.push(Integer.valueOf(value));
+            queue.add(new Token(TokenType.NUM, value).toString());
+            continue;
+
+           }
             switch(value){
                 case "+":
                     op1 = stack.pop();
                     op2 = stack.pop();
                     stack.push(op1+op2);
+                    queue.add(new Token(TokenType.PLUS, value).toString());
                     break;
                 case "-":
                     op2 = stack.pop();
                     op1 = stack.pop();
                     stack.push(op1-op2);
+                    queue.add(new Token(TokenType.MINUS, value).toString());
                     break;
                 case "/":
                     op2 = stack.pop();
                     op1 = stack.pop();
                     stack.push(op1/op2);
+                    queue.add(new Token(TokenType.SLASH, value).toString());
                     break;
                 case "*":
                 op1 = stack.pop();
                 op2 = stack.pop();
                 stack.push(op1*op2);
+                queue.add(new Token(TokenType.STAR, value).toString());
                 break;
                 default:
-                    stack.push(Integer.valueOf(value));
+                    throw new Exception("Unexpected value: " + value);
             }
         }
-        System.out.println("O resultado da sua operação é: " + stack.pop());
-
+        System.out.println(queue);
+        System.out.println("Saída: " + stack.pop());
     }
 }
---
+++ TokenType.java
@@ -0,0 +1,9 @@
+public enum TokenType {
+    // Literals.
+	NUM,
+
+	// Single-character tokens for operations.
+	MINUS, PLUS, SLASH, STAR,
+	
+	EOF
+}
